6f16b30059447aed0824eaadfcde573866b61dbc
Hung Tran <hungtran511997@gmail.com>
2023-04-10 02:13:56 +0700

    remove check plugin for color schema

--- lua/colors.lua
+++ lua/colors.lua
@@ -5,11 +5,6 @@
 -- local colorscheme = "catppuccin"
 local colorscheme = "nightfox"
 
-local status_ok, _ = pcall(require, "plugins." .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " doesn't have a setup file!")
-end
-
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
