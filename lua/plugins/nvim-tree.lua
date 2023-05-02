30318b9b7861f54b67ca8c907b19310af814e15d
Hung Tran <hungtran511997@gmail.com>
2023-05-02 13:17:28 +0700

    chore: remove deprecated config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -57,17 +57,9 @@ nvim_tree.setup {
   hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
   open_on_tab = false,
   sort_by = "name",
   update_cwd = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   view = {
     width = 30,
     hide_root_folder = false,
